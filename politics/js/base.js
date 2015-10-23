75dc3a7414d9b5d587739742b39717bc24fdc6ad
haynes1 <haynes1@purdue.edu>
2015-10-23 12:47:39 -0400

    next day

--- politics/js/base.js
+++ politics/js/base.js
@@ -5,14 +5,72 @@ function expandNav(){
 	    	$('#nav_expand').attr('onclick', 'collapseNav()')
 	    	console.log('now collapsable')
 	    });
+	    $.scrollLock();
 }
 
 function collapseNav(){
 	h = -1 * $('nav_container').height()
 	$('#nav_container').animate({
-        top: '-260px',
+        top: '-100vh',
     }, 500, function(){
     	$('#nav_expand').attr('onclick', 'expandNav()')
     	$('#badge').css('display', '');
     });
+    $.scrollLock();
 }
+
+function submarine(){
+	console.log($(this).parent())
+
+}
+function hotair(){
+	
+}
+function implode(){
+	
+}
+
+$.scrollLock = ( function scrollLockSimple(){
+	var locked   = false;
+	var $body;
+	var previous;
+
+	function lock(){
+	  if( !$body ){
+	    $body = $( 'body' );
+	  }
+	  
+	  previous = $body.css( 'overflow' );
+		
+	  $body.css( 'overflow', 'hidden' );
+
+	  locked = true;
+	}
+
+	function unlock(){
+	  $body.css( 'overflow', previous );
+
+	  locked = false;
+	}
+
+	return function scrollLock( on ) {
+		// If an argument is passed, lock or unlock depending on truthiness
+		if( arguments.length ) {
+			if( on ) {
+				lock();
+			}
+			else {
+				unlock();
+			}
+		}
+		// Otherwise, toggle
+		else {
+			if( locked ){
+				unlock();
+			}
+			else {
+				lock();
+			}
+		}
+	};
+}() );
