c9a5fd948d9f688a92ff24285f6f92f7a9291d00
haynes1 <haynes1@purdue.edu>
2015-12-15 21:12:06 -0500

    source navigation still isn't responsive. overall, like the rest of the site, it needs something major

--- politics/js/publicbase.js
+++ politics/js/publicbase.js
@@ -49,3 +49,13 @@ function submitSign() {
     console.log(data)
   });
 }
+
+function scrollTo(id){
+  if (id){
+    scrollpos = $("#"+id).offset().top - 100;
+    console.log($("#"+id).offset().top)
+    $('html, body').animate({
+          scrollTop: scrollpos
+      }, 500);
+  }
+}
