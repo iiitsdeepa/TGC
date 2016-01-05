a5debbb7ee40c4605ee6084b9664630355354d78
Alex Haynes <haynes1@purdue.edu>
2016-01-05 14:57:22 -0500

    changed classes to include poptype and mode

--- Glass/polling/historicsanitizer.py
+++ Glass/polling/historicsanitizer.py
@@ -1,15 +1,17 @@
 #this program converts the raw txt files of congressional members, and converts them to CSV files of just first and last name
 
 f = open('2016-national-gop-primary.csv')
+n = open('111membs.csv', 'w')
 #n = open('111membs.csv', 'w')
 linenum = 0
 for l in iter(f):
 	if linenum != 0:
 		u = l.split(',')
-		for d in u:
-			if d == '':
-				d = '-1'
-		print u
+		for i in xrange(len(u)):
+			if u[i] == '':
+				u[i] = '-1'
+		line = ','.join(map(str, u))
+		print line
 	linenum += 1
 
 print linenum
