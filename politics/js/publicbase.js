b12d41f7c391c4288737ecb059e9e1793b586fe3
Alex Haynes <haynes1@purdue.edu>
2016-01-09 16:54:35 -0500

    centered the capitol building and put in the old header bar on the interactives page

--- politics/js/publicbase.js
+++ politics/js/publicbase.js
@@ -1,11 +1,48 @@
 function expandGlobalNav(){
   //expand
-  if ($('#expandnav').attr('value') == 'contracted'){
-    $('#nav_wrapper').animate({left:"-100vw"}, 200, function(){$('#expandnav').attr('value','expanded')})
+  if ($('#nav_expand').attr('value') == 'contracted'){
+    console.log('expanding nav')
+    $('#nav_container').animate({top:"0"}, 350, function(){$('#nav_expand').attr('value','expanded')})
   } else{ //contract
-    $('#nav_wrapper').animate({left:"0px"}, 200, function(){$('#expandnav').attr('value','expanded')})
+    console.log('contracting nav')
+    $('#nav_container').animate({top:"-100vh"}, 350, function(){$('#nav_expand').attr('value','contracted')})
   }
 }
+function expandPagenav(){
+    $('#pagenav').animate({
+          top: '40px',
+      }, 300, function(){
+        $('#pagenav_expand').attr('onclick', 'collapsePagenav()')
+      });
+}
+function collapsePagenav(i){
+  $('#pagenav').animate({
+        top: '-50vh',
+    }, 300, function(){
+      $('#pagenav_expand').attr('onclick', 'expandPagenav()')
+      $('#badge').css('display', '');
+    });
+}
+function implode(){
+  $('.mexp').html('>')
+  $('.nav_option').attr('value','contracted');
+  $('nav ul li').css('height', '40px')
+}
+function submarine(li, bt){
+  //expand the subnav
+  if ($('#'+li).attr('value') == 'contracted'){
+    implode();
+    $('#'+li).attr('value', 'expanded');
+    $('#'+li).css('background-color', '')
+    $('#'+bt).html('-')
+    $('#'+li).css('height','');
+  }
+  //contract the subnav
+  else{
+    $('#'+li).attr('value', 'contracted');
+    implode();
+  }
+};
 
 function alphaExpand(a, t){
 	//expand it
@@ -68,5 +105,11 @@ function scrollTo(id){
       }, 500);
   }
 }
+
+$( document ).ready(function() {
+    implode();
+    $('#pagenav_expand').attr('onclick', 'expandPagenav()');
+});
+
 
 
