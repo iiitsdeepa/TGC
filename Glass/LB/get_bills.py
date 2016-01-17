3a02badb93fba288e68dc9515d4e016eca72d1cd
Alex Haynes <haynes1@purdue.edu>
2016-01-17 03:13:12 -0500

    working get bills

--- Glass/LB/get_bills.py
+++ Glass/LB/get_bills.py
@@ -3,48 +3,69 @@ import json
 from time import sleep
 
 #opening log file
-csv = open('bills.csv', 'w')
+csv = open('bills112.csv', 'w')
 
 congress = 112
 chambers=['hr','s']
-bill_url = 'https://congress.api.sunlightfoundation.com/bills?fields=bill_id,official_title,popular_title,short_title,nicknames,keywords,summary_short,urls,history.active,history.vetoed,history.vetoed_at,history.enacted,history.enacted_at,sponsor_id,cosponsor_ids,cosponsor_count,keywords&congress=%s&bill_type=%s&per_page=50&apikey=5a2e18d2e3ed4861a8604e9a5f96a47a'
-for c in chambers
-	u = urllib2.urlopen(bill_url % (congress, 's'))
+bill_url = 'https://congress.api.sunlightfoundation.com/bills?fields=bill_id,official_title,popular_title,short_title,nicknames,keywords,summary_short,urls,history.active,history.vetoed,history.vetoed_at,history.enacted,history.enacted_at,sponsor_id,cosponsor_ids,cosponsor_count,keywords&congress=%s&bill_type=%s&per_page=50&%sapikey=5a2e18d2e3ed4861a8604e9a5f96a47a'
+total_count = 0
+for c in chambers:
+	u = urllib2.urlopen(bill_url % (congress, c,'page=1&'))
 	b = u.read()
 	j = json.loads(b)
 
-	total_count = j["count"]
-	ra = j["results"]
-	for r in ra:
-		bid = r["bill_id"]
-		official_title = r["official_title"]
-		popular_title = r["popular_title"]
-		short_title = r["short_title"]
-		try:
-			nicknamesa = r["nicknames"]
-			nicknames = '$$$'.join(nicknamesa)
-		except:
-	   		nicknames = 'null'
-	   	try:
-			keywordsa = r["keywords"]
-			keywords = '$$$'.join(keywordsa)
-			#keywords = "$$$".join(keywords)
-		except:
-	   		keywords = 'null'
-	   	url = r["urls"]["congress"]
-	   	active = r["history"]["active"]
-	   	vetoed = r["history"]["vetoed"]
-	   	enacted = r["history"]["enacted"]
-	   	sponsor = r["sponsor_id"]
-	   	try:
-			cosponsorsa = r["cosponsor_ids"]
-			cosponsors = '$$$'.join(cosponsorsa)
-		except:
-	   		cosponsors = 'null'
+	total_count += int(j["count"])
+	per_page = int(j["page"]["per_page"])
+	num_pages = int(j["count"])/per_page
+	print total_count,per_page,num_pages
+	for x in range(1,num_pages+1):
+		page = 'page='+str(x)+'&'
+		u = urllib2.urlopen(bill_url % (congress, c, page))
+		b = u.read()
+		j = json.loads(b)
+		ra = j["results"]
+		print 'page '+str(x)
+		for r in ra:
+			try:
+				bid = r["bill_id"].encode('utf-8')
+			except:
+				bid = '-1'
+			try:
+				official_title = r["official_title"].encode('utf-8')
+			except:
+				official_title = 'None'
+			try:
+				popular_title = r["popular_title"].encode('utf-8')
+			except:
+				popular_title = 'None'
+			try:
+				short_title = r["short_title"].encode('utf-8')
+			except:
+				short_title = 'None'
+			try:
+				nicknamesa = r["nicknames"].encode('utf-8')
+				nicknames = '$$$'.join(nicknamesa)
+			except:
+		   		nicknames = 'None'
+		   	try:
+				keywordsa = r["keywords"].encode('utf-8')
+				keywords = '$$$'.join(keywordsa)
+				#keywords = "$$$".join(keywords)
+			except:
+		   		keywords = 'Nonel'
+		   	url = r["urls"]["congress"].encode('utf-8')
+		   	active = str(r["history"]["active"])
+		   	vetoed = str(r["history"]["vetoed"])
+		   	enacted = str(r["history"]["enacted"])
+		   	sponsor = r["sponsor_id"].encode('utf-8')
+		   	try:
+				cosponsorsa = r["cosponsor_ids"].encode('utf-8')
+				cosponsors = '$$$'.join(cosponsorsa)
+			except:
+		   		cosponsors = 'None'
+		   	line = bid+','+official_title+','+popular_title+','+short_title+','+nicknames+','+url+','+active+','+vetoed+','+enacted+','+sponsor+','+cosponsors+','+keywords+'\n'
+			csv.write(line)
+		sleep(1.00)
 
 
-	   	print bid,official_title,popular_title,short_title,url,active,vetoed,enacted,sponsor,cosponsors
-
-
-
-csv.write(json.dumps(j,indent=4))
+print total_count
