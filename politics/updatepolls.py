801e0db859f25268c172577dfa8ebb706c05dfb4
Steven <thatguysch@gmail.com>
2016-01-26 22:59:36 -0500

    fucking problem with huff post solved

--- politics/updatepolls.py
+++ politics/updatepolls.py
@@ -37,13 +37,12 @@ def getNationalPolls():
 
     dem_url = 'http://elections.huffingtonpost.com/pollster/api/polls.json?%stopic=2016-president-dem-primary'
     gop_url = 'http://elections.huffingtonpost.com/pollster/api/polls.json?%stopic=2016-president-gop-primary'
-    tempgopdate = GqlQuery("SELECT * FROM NationalRepublicanPrimary ORDER BY end_date DESC").get()
+    tempgopdate = GqlQuery("SELECT * FROM NationalRepublicanPrimary ORDER BY entry_date DESC").get()
     try:
-        topdategop = tempgopdate.end_date
+        topdategop = tempgopdate.entry_date
     except:
         topdategop = datetime.min
-    logging.error(str(topdategop))
-    todaydate = datetime.today()
+    #logging.error(str(topdategop))
     breakvar = False
     #this for loop goes through each page in the api database, starting at page = 1 (most recent) and going to the end (earliest)
     for x in range(1,100):
@@ -61,11 +60,12 @@ def getNationalPolls():
             notend = i["end_date"]
             notstart = i["start_date"]
             method = i["method"]
-            logging.error(str(polls)+" "+str(notend))
+            #logging.error(str(polls)+" "+str(notend))
             start = datetime.strptime(notstart, '%Y-%m-%d')
             end = datetime.strptime(notend, '%Y-%m-%d')
-            if (end < topdategop):
-                logging.error('breaking at '+str(polls))
+            lastupdated = datetime.strptime(i["last_updated"], '%Y-%m-%dT%H:%M:%S.%fZ')
+            if (lastupdated <= topdategop):
+                #logging.error('breaking at '+str(polls))
                 breakvar = True
                 break
             sourceurl = i["source"]
@@ -103,21 +103,20 @@ def getNationalPolls():
                             chri = int(k["value"])
                         if (k["choice"] == "Carson"):
                             car = int(k["value"])
-                    if (end > topdategop):
-                        entry = NationalRepublicanPrimary(pollster=polls, start_date=start, end_date=end, entry_date=todaydate, popsize=pop, poptype=poptype, mode=method, trump=tru, cruz=cru, rubio=rub, kasich=kas, carson=car, bush=bus, christie=chri, paul=pau, fiorina=fio, huckabee=huc, santorum=sant, gilmore=gil, gram=gra, jindal=jin, pataki=pat, perry=per, walker=wal, undecided=rundec, url=sourceurl)
-                        entry.put()
-                        logging.error('put entry: '+" "+str(polls))
+                    entry = NationalRepublicanPrimary(pollster=polls, start_date=start, end_date=end, entry_date=lastupdated, popsize=pop, poptype=poptype, mode=method, trump=tru, cruz=cru, rubio=rub, kasich=kas, carson=car, bush=bus, christie=chri, paul=pau, fiorina=fio, huckabee=huc, santorum=sant, gilmore=gil, gram=gra, jindal=jin, pataki=pat, perry=per, walker=wal, undecided=rundec, url=sourceurl)
+                    entry.put()
+                    #logging.error('put entry: '+" "+str(polls))
         if (breakvar == True):
             break
         sleep(1.00)
 
     #this for loop goes through each page in the api database, starting at page = 1 (most recent) and going to the end (earliest)
-    tempdemdate = GqlQuery("SELECT * FROM NationalDemocraticPrimary ORDER BY end_date DESC").get()
+    tempdemdate = GqlQuery("SELECT * FROM NationalDemocraticPrimary ORDER BY entry_date DESC").get()
     try:
-        topdatedem = tempdemdate.end_date       
+        topdatedem = tempdemdate.entry_date       
     except:
         topdatedem = datetime.min 
-    logging.error(str(topdatedem))
+    #logging.error(str(topdatedem))
     breakvar = False
     for x in range(1,100):
         logging.error('DEM page: '+str(x))
@@ -133,11 +132,12 @@ def getNationalPolls():
             polls = i["pollster"]
             notend = i["end_date"]
             notstart = i["start_date"]
-            logging.error(str(polls)+" "+str(notend))
+            #logging.error(str(polls)+" "+str(notend))
             start = datetime.strptime(notstart, '%Y-%m-%d')
             end = datetime.strptime(notend, '%Y-%m-%d')
-            if (end < topdatedem):
-                logging.error('breaking at '+str(polls))
+            lastupdated = datetime.strptime(i["last_updated"], '%Y-%m-%dT%H:%M:%S.%fZ')
+            if (lastupdated <= topdatedem):
+                #logging.error('breaking at '+str(polls))
                 breakvar = True
                 break
             method = i["method"]
@@ -158,10 +158,9 @@ def getNationalPolls():
                             sand = int(k["value"])
                         if (k["choice"] == "O'Malley"):
                             omal = int(k["value"])
-                    if (end > topdatedem):
-                        entry = NationalDemocraticPrimary(pollster=polls, start_date=start, end_date=end, entry_date=todaydate, popsize=pop, poptype=poptype, mode=method, hill=cli, sanders=sand, omalley=omal, chafee=cha, webb=web, biden=bid, undecided=dundec, url=sourceurl)
-                        entry.put()
-                        logging.error('put entry: '+" "+str(polls))
+                    entry = NationalDemocraticPrimary(pollster=polls, start_date=start, end_date=end, entry_date=lastupdated, popsize=pop, poptype=poptype, mode=method, hill=cli, sanders=sand, omalley=omal, chafee=cha, webb=web, biden=bid, undecided=dundec, url=sourceurl)
+                    entry.put()
+                    #logging.error('put entry: '+" "+str(polls))
         if (breakvar == True):
             break
         sleep(1.00)
