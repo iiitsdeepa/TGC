b85e79e9fafa955cdaaea9ab9564ca2a676f8e49
Alex Haynes <haynes1@purdue.edu>
2016-02-12 13:12:01 -0500

    its starting

--- politics/politics.py
+++ politics/politics.py
@@ -351,7 +351,7 @@ class Feedback(BaseHandler):
 
 class Vprop(BaseHandler):
     def get(self):
-        self.render('interactives.html')
+        self.render('new-feedback.html')
 
     def post(self):
         demo = self.request.get('demo')
